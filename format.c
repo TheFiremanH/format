3f9361be60f5d871a9ab13c69b0100fb4500119e
Hippolyte Melica <hippolytemelica@gmail.com>
2021-02-13 15:16:34 +0100

    fonctionnel

--- format.c
+++ format.c
@@ -29,6 +29,7 @@ int main(void)
 	
 	int caracterePrecedent = 0;
 	int caractereActuel = 0;
+	int caractereTransfert = 0;
 	while (modifsTmp != 0)
 	{
 		passages++;
@@ -38,61 +39,48 @@ int main(void)
 		fichier = fopen(nomDeFichier, "r+");
 		if (temp == NULL || fichier == NULL) 
 		{ 
-			printf("\nUnable to create temp file"); 
+			printf("\nUnable to create files"); 
 			return 0; 
 		}
 		rewind(temp);
 		do
 		{
 			caractereActuel = fgetc(fichier);
-			printf("\nChecking %d (%c), previously checking %d (%c)", caractereActuel, caractereActuel, caracterePrecedent, caracterePrecedent);
-			if (caractereActuel != EOF)
+			if (caractereActuel == '\n') 
 			{
-				if (caractereActuel == '\n') 
+				if (caracterePrecedent == ' ') // si le caractere avant un retour est un espace
 				{
-					if (caracterePrecedent == ' ') // si le caractere avant un retour est un espace
-					{
-						recordIt = false; // ne pas l'enregistrer
-					}
-					if (caracterePrecedent == '\t')
-					{
-						recordIt = false; // ne pas l'enregistrer 
-					}
+					recordIt = false; // ne pas l'enregistrer
+					modifs++;
+					modifsTmp++;
 				}
-				if (caracterePrecedent == 255)
+				if (caracterePrecedent == '\t')
 				{
-					recordIt = false;
+					recordIt = false; // ne pas l'enregistrer 
+					modifs++;
+					modifsTmp++;
 				}
+			}
+			if (caracterePrecedent == 255)
+			{
+				recordIt = false;
+			}
 
-				if (recordIt)
-				{
-					fputc(caracterePrecedent, temp);
-					printf("\n vvv Caractere %d (%c) was saved", caracterePrecedent, caracterePrecedent);
-				}
-				else
-				{
-					printf("\n xxx Caractere %d (%c) was not saved", caracterePrecedent, caracterePrecedent);
-					if (caracterePrecedent == ' ' || caracterePrecedent == '\t')
-					{
-						modifs++;
-						modifsTmp++;
-					}
-				}
-				recordIt = true;
-				caracterePrecedent = caractereActuel;
+			if (recordIt)
+			{
+				fputc(caracterePrecedent, temp);
+				//printf("\n vvv Caractere %d (%c) was saved", caracterePrecedent, caracterePrecedent);
 			}
 			else
 			{
-				printf("\nEnd of document detected, saving previous charactere");
-				fputc(caracterePrecedent, temp);
-				break;
+				//printf("\n xxx Caractere %d (%c) was not saved", caracterePrecedent, caracterePrecedent);
 			}
+			recordIt = true;
+			caracterePrecedent = caractereActuel;
 		} while (caractereActuel != EOF);
 
-		printf("\nSaving temp file to file");
 		rewind(fichier);
 		fclose(fichier);
-		printf("\n  > Original file is being recycled.");
 		fichier = fopen(nomDeFichier, "w");
 		if (fichier == NULL) 
 		{ 
@@ -101,19 +89,26 @@ int main(void)
 		}
 		rewind(temp);
 		rewind(fichier);
-		printf("\n  > Writing the modifications");
+		//printf("\n > Writing the modifications :");
 		while(!feof(temp))
 		{
-			fputc(fgetc(temp), fichier);
+			caractereTransfert = fgetc(temp);
+			if (caractereTransfert >= 0)
+			{
+				//printf("\n    >> %c (%d)", caractereTransfert, caractereTransfert);
+				fputc(caractereTransfert, fichier);
+			} 
+			else 
+			{
+				//printf("\n   xx> %c (%d)", caractereTransfert, caractereTransfert);
+			}
 		}
 		rewind(temp);
 		fclose(temp);
 		rewind(fichier);
 		fclose(fichier);
-		printf("\n\nTempfile saved. modifsTmp = %d", modifsTmp);
 	}
-	printf("\n%d characteres were deleted.\n", modifs);
+	printf("\n\n%d characteres were deleted.\nThe file was used %d times\n\n", modifs, passages);
 
 	return 0;
 }
-
