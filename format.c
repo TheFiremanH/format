f09aa21ee03a517f9ea426743612eadd5b757577
Hippolyte Melica <hippolytemelica@gmail.com>
2021-05-01 15:39:25 +0200

    Changement du message

--- format.c
+++ format.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		printf("\nArguments required !\n  format file_name -n\n");
+		printf("\nArguments required !\n  format file_name (-n)\n");
 		return 0;
 	}
 	int modifs = 0;
